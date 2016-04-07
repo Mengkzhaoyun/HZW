5dad1db376e8a5f8a22c47aecb7bfcb55eda18a4
mengkzhaoyun <mengkzhaoyun@gmail.com>
2016-04-07 17:50:52 +0800

    将html的视图后缀更换为tpl

--- app.js
+++ app.js
@@ -18,8 +18,8 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'html');
-app.engine('html', swig.renderFile);
+app.set('view engine', 'tpl');
+app.engine('tpl', swig.renderFile);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
